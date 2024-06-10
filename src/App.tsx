b74d06e982bf818e72314c0129469449d106676c
raaghav03 <raghavnagpal2003@gmail.com>
2024-06-10 22:38:21 +0530

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import Sidebar from "../src/components/Sidebar";
 import Topbar from "../src/components/Topbar";
-<<<<<<< HEAD
-import React from "react";
-=======
->>>>>>> 399399c (initial commit)
+
 
 export default function App() {
   return (
